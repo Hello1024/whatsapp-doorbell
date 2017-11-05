1d2cb223c2946aad9cc9a6f4f9b6dcb9abb6a5f7
Your Name <you@example.com>
2017-11-05 15:14:53 +0000

    Light control!

--- yowsup/demos/echoclient/layer.py
+++ yowsup/demos/echoclient/layer.py
@@ -5,6 +5,15 @@ import os
 import logging
 import picamera
 import subprocess
+from threading import Timer
+from dateutil import parser
+import datetime
+import requests
+import json
+
+from astral import Astral
+import pytz
+
 from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
 from yowsup.layers.protocol_iq.protocolentities          import *
 from yowsup.layers.protocol_media.protocolentities       import *
@@ -32,18 +41,35 @@ class EchoLayer(YowInterfaceLayer):
       def doorbell_pressed(channel):
           logging.info('doorbell_presed')
           self.sendMsg("Someones at the door!!!")
+          self.setLight(on=True, bri=254, off_after=5);
           self.sendPic()
           self.belltime = time.time()
           
 
       def door_opened(channel):
           logging.info('dooropened')
+          self.setLight(on=True, bri=254, off_after=100, if_dark=True);
+          
           if (time.time() - self.belltime < 100): 
             self.sendMsg("Got it!")
             self.belltime = 0
+      
+      def alarmHandler():
+        self.sendMsg("1pm Alarm!!", to = Jid.normalize('447760333610'))
+        # set tomorrows alarm
+        setAlarm();
+        
+      def setAlarm():
+        setAnAlarm("1:00PM", alarmHandler);
 
+      def setAnAlarm(time, handler):
+        Timer((parser.parse(time) - datetime.datetime.now()).seconds+1, handler).start()  
+
+      setAlarm()
+      self.setLight(on=False);
       GPIO.add_event_detect(12, GPIO.RISING, callback=door_opened, bouncetime=3000)
       GPIO.add_event_detect(16, GPIO.FALLING, callback=doorbell_pressed, bouncetime=3000)
+      
 
     @ProtocolEntityCallback("success")
     def onSuccess(self, successProtocolEntity):
@@ -51,8 +77,6 @@ class EchoLayer(YowInterfaceLayer):
         
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
-        
-
         self.toLower(messageProtocolEntity.ack())
         self.toLower(messageProtocolEntity.ack(True))
         
@@ -81,7 +105,7 @@ class EchoLayer(YowInterfaceLayer):
           self.sendMsg("No lumberjacks allowed!!!", to=reply_to)
         
         if 'echo' == text.split(' ')[0]:
-          self.sendMsg(messageProtocolEntity.getBody()[5:], to=reply_to)
+          self.sendMsg(text[5:], to=reply_to)
           
         if '14' == text:
           self.sendMsg('start the game, already!', to=reply_to)
@@ -98,6 +122,14 @@ class EchoLayer(YowInterfaceLayer):
         if 'uptime' in text:
           self.sendMsg(subprocess.check_output('uptime', shell=True), to=reply_to)
 
+        if 'light on' == text:
+          self.setLight(on=True, bri=254)
+          self.sendMsg('Your will is my command!', to=reply_to)
+
+        if 'light off' == text:
+          self.setLight(on=False)
+          self.sendMsg('Your will is my command! (even though you\'ve left me in the dark :-( )', to=reply_to)
+
         #self.toLower(messageProtocolEntity.forward(messageProtocolEntity.getFrom()))
         
 
@@ -111,14 +143,13 @@ class EchoLayer(YowInterfaceLayer):
         self.toLower(messageEntity)
         
     def sendPic(self, to = Jid.normalize('447760333610-1485190753')):
-          #os.system('raspistill -o img.jpg')
-          self.camera.capture('img.jpg')
-          path='img.jpg'
-          mediaType = RequestUploadIqProtocolEntity.MEDIA_TYPE_IMAGE
-          entity = RequestUploadIqProtocolEntity(mediaType, filePath=path)
-          successFn = lambda successEntity, originalEntity: self.onRequestUploadResult(to, mediaType, path, successEntity, originalEntity, None)
-          errorFn = lambda errorEntity, originalEntity: self.onRequestUploadError(to, path, errorEntity, originalEntity)
-          self._sendIq(entity, successFn, errorFn)
+        self.camera.capture('img.jpg')
+        path='img.jpg'
+        mediaType = RequestUploadIqProtocolEntity.MEDIA_TYPE_IMAGE
+        entity = RequestUploadIqProtocolEntity(mediaType, filePath=path)
+        successFn = lambda successEntity, originalEntity: self.onRequestUploadResult(to, mediaType, path, successEntity, originalEntity, None)
+        errorFn = lambda errorEntity, originalEntity: self.onRequestUploadError(to, path, errorEntity, originalEntity)
+        self._sendIq(entity, successFn, errorFn)
  
     def onRequestUploadResult(self):
       pass
@@ -142,3 +173,15 @@ class EchoLayer(YowInterfaceLayer):
     def doSendMedia(self, mediaType, filePath, url, to, ip = None, caption = None):
           entity = ImageDownloadableMediaMessageProtocolEntity.fromFilePath(filePath, url, ip, to, caption = caption)
           self.toLower(entity)
+
+    def setLight(self, if_dark=False, off_after=0, **kwargs):
+      night_info = Astral()['London'].sun()
+      now = datetime .datetime.now(pytz.utc)
+
+      if now >= night_info['dusk'] or now <= night_info['dawn'] or not if_dark:
+        requests.put('http://househue.lan/api/MIjBaDjRTik4hz2lZKQmAB6JOgpNvdkKXp1mJeBT/lights/1/state', data=json.dumps(kwargs, default=list))
+        logging.info('turning on/off light')
+        if off_after:
+          Timer(off_after, self.setLight, kwargs={'on':False}).start();
+      else:
+        logging.info('not dark, so no light')
